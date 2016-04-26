0ffdbdf103044de0c80278d7459f1a1a3fc16f96
elliotfiske <elliotfiske@gmail.com>
2016-04-26 13:27:38 -0700

    DELETION TIME

--- skylink/demo/config-example.js
+++
@@ -1,8 +0,0 @@
-/**************************** Note **********************************
- Save your api settings like apiKey, defaultRoom and room and save it
- in a file called [config.js]
-*********************************************************************/
-var config = {
-  apiKey:"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
-  defaultRoom:"MY_DEFAULT_ROOM"
-};
--- skylink/source/template/footer.js
+++
@@ -1,2 +0,0 @@
-window.Skylink = Skylink;
-}).call(this);
